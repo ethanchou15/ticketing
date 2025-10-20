f7f8b682e8234d970d5974be046fbdca3f7f571e
Ethan Chou <yqu82888@gmail.com>
2025-10-20 14:50:28 -0400

    added skaffold and ingress nginx setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,10 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("hi there!!!");
+});
+
 app.listen(3000, () => {
-  console.log("Auth service running on port 3000");
+  console.log("Auth service running on port 3000!!!");
 });
