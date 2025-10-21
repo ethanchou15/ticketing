ef789455e6d3ca413d010d0ea1744e5b4d29c71f
Ethan Chou <yqu82888@gmail.com>
2025-10-20 21:17:45 -0400

    configure AKS

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,7 +3,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/api/users/currentuser", (req, res) => {
-  res.send("hi there!!!");
+  res.send("hi there");
 });
 
 app.listen(3000, () => {
