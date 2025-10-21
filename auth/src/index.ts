fffa3b724c72348621e4d76e0d5bcc8b9e150250
Ethan Chou <yqu82888@gmail.com>
2025-10-21 11:36:03 -0400

    added not found error

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,10 @@
 import express, { Express } from "express";
+import { NotFoundError } from "./errors/not-found-error.ts";
+import { errorHandler } from "./middlewares/error-handler.ts";
 import currentUserRouter from "./routes/current-user.ts";
 import signinRouter from "./routes/signin.ts";
 import signoutRouter from "./routes/signout.ts";
 import signupRouter from "./routes/signup.ts";
-import { errorHandler } from "./middlewares/error-handler.ts";
 const app: Express = express();
 app.use(express.json());
 
@@ -11,6 +12,11 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+// Handle all other routes /api/users/* that are not defined
+app.all("/{*any}", () => {
+  throw new NotFoundError();
+});
 
 app.use(errorHandler);
 
