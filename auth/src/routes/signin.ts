929fb8bda47e395e865fe4f2240d1a839ea98e04
Ethan Chou <yqu82888@gmail.com>
2025-10-22 11:44:08 -0400

    signin flow

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,8 +1,23 @@
 import { Router, Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-error";
 const router = Router();
 
-router.post("/api/users/signin", (req: Request, res: Response) => {
-  res.send("hi there!");
-});
+router.post(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("You must supply a password"),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
+  }
+);
 
 export default router;
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -8,11 +8,13 @@ const router = Router();
 
 router.post(
   "/api/users/signup",
-  body("email").isEmail().withMessage("Email must be valid"),
-  body("password")
-    .trim()
-    .isLength({ min: 4, max: 20 })
-    .withMessage("Password must be between 4 and 20 characters"),
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
+  ],
   async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
