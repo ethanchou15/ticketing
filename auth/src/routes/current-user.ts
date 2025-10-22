cc1c734118e139df0ea6a655fa29fda9bd0a5043
Ethan Chou <yqu82888@gmail.com>
2025-10-22 14:09:14 -0400

    current user handler

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,8 +1,19 @@
 import { Router, Request, Response } from "express";
+import jwt from "jsonwebtoken";
 const router = Router();
 
 router.get("/api/users/currentuser", (req: Request, res: Response) => {
-  res.send("hi there!!!");
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  try {
+    // payload is the decoded JWT token, including id and email
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.send({ currentUser: payload });
+  } catch (err) {
+    res.send({ currentUser: null });
+  }
 });
 
 export default router;
