2e3deb2af5af2eb69a9de597a963a0119ca2b415
Ethan Chou <yqu82888@gmail.com>
2025-10-22 11:57:52 -0400

    signin logic

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,10 @@
 import { Request, Response, Router } from "express";
 import { body } from "express-validator";
 import { validateRequest } from "../middlewares/validate-request";
+import { User } from "../models/user";
+import { BadRequestError } from "../errors/bad-request-error";
+import { Password } from "../services/password";
+import jwt from "jsonwebtoken";
 const router = Router();
 
 router.post(
@@ -14,7 +18,36 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
-    
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError("Invalid credentials");
+    }
+
+    const passwordsMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+    if (!passwordsMatch) {
+      throw new BadRequestError("Invalid credentials");
+    }
+
+    // generate JWT token
+    const userJwt = jwt.sign(
+      {
+        // id -> string, _id -> ObjectId
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    // store it on session object, name is session, the value is the object(jwt: token)
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(200).send(existingUser);
   }
 );
 
