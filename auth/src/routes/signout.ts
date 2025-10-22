bef5980b1325146cb7951bdc83831835652ade8d
Ethan Chou <yqu82888@gmail.com>
2025-10-22 14:12:25 -0400

    sign out

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -1,8 +1,10 @@
-import { Router, Request, Response } from "express";
+import { Request, Response, Router } from "express";
 const router = Router();
 
 router.post("/api/users/signout", (req: Request, res: Response) => {
-  res.send("hi there!");
+  // clear the session object
+  req.session = null;
+  res.send({});
 });
 
 export default router;
