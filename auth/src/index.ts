e4217097a30cab6d0a61ecb2b13fe9c729135d36
Ethan Chou <yqu82888@gmail.com>
2025-10-22 21:33:09 -0400

    index to app.ts refactor

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,37 +1,5 @@
-import express, { Express } from "express";
-import { NotFoundError } from "./errors/not-found-error.ts";
-import { errorHandler } from "./middlewares/error-handler.ts";
-import currentUserRouter from "./routes/current-user.ts";
-import signinRouter from "./routes/signin.ts";
-import signoutRouter from "./routes/signout.ts";
-import signupRouter from "./routes/signup.ts";
 import { connect } from "mongoose";
-import cookieSession from "cookie-session";
-const app: Express = express();
-
-app.use(express.json());
-
-// trust traffic from ingress-nginx proxy, so that secure cookies work
-app.set("trust proxy", true);
-// configure cookie session middleware, so that we can use req.session(default name is session)
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-// Handle all other routes /api/users/* that are not defined
-app.all("/{*any}", async () => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import { app } from "./app.ts";
 
 const start = async () => {
   // check if JWT_KEY is defined
